f5c2bd9c77973c0240f312aa4949702d0fdbb17a
Wiola-D <wioletta.d.work@gmail.com>
2024-02-05 18:11:10 +0100

    add hw2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Vimeo from '@vimeo/player';
+//const { Player } = require('@vimeo/player');
+var throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Vimeo(iframe);
+
+player.on('play', () => console.log('played the video'));
+
+player.on(
+  'timeupdate',
+  throttle(data => {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+  }, 1000)
+);
+
+const timeUpgrade = localStorage.getItem('videoplayer-current-time');
+
+if (timeupdate) {
+  player.setCurrentTime(timeUpgrade);
+}
